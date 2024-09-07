7b587ebe85f84f06ee18b672b79a9539987222a2
timmypelumy <timmypelumy@gmail.com>
2024-09-07 17:19:26 +0100

    dashboard page done

---
+++ src/components/bluroverlay.jsx
@@ -0,0 +1,5 @@
+export default function BlurOverlay() {
+  return (
+    <div className="fixed inset-0 bg-white/5 backdrop-blur-[2px] bg-opacity-50 z-20"></div>
+  );
+}
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -11,6 +11,7 @@ import {
 import { XAxis, YAxis, AreaChart, Tooltip, Area } from "recharts";
 import { useState } from "react";
 import cn from "classnames";
+import BlurOverlay from "../components/bluroverlay";
 
 const data = [
   {
@@ -80,6 +81,29 @@ export default function Dashboard() {
 
   return (
     <div className="w-full min-h-screen ">
+      {/* overlay  */}
+
+      <BlurOverlay />
+
+      <div className="rounded bg-white center fixed top-1/2 left-1/2 z-20 drop-shadow-xl -translate-y-1/2 -translate-x-1/2 p-6 w-full max-w-md min-h-[30vh] space-y-4 border">
+        <h1 className="text-xl text-primary font-bold">Welcome</h1>
+
+        <p className="text-[#000000CC] text-sm text-center">
+          To begin accepting payments on ChainPal, you are required to complete
+          your KYC verification. This step is necessary to ensure compliance
+          with regulatory standards and to provide you with a secure and
+          seamless payment experience.
+        </p>
+
+        <button className="btn-1 w-[50%] px-6 py-2 text-sm ">
+          Get Verified
+        </button>
+
+        <p className="text-[#000000CC] text-sm text-center cursor-pointer">
+          Skip for later?
+        </p>
+      </div>
+
       <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
 
       <div
